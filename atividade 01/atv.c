d0cf804a5a4b675add0787e2890044119764d86f
pedro-hma <105680915+pedro-hma@users.noreply.github.com>
2024-09-02 10:51:13 +0000

    erro consertado

--- atividade 01/atv.c
+++ atividade 01/atv.c
@@ -1,35 +1,39 @@
-# include <stdio.h>
+#include <stdio.h>
 
-void ordenacao(int v[],int qtd,int valor){
+void ordenacao(int v[], int qtd, int valor) {
     int i = qtd - 1;
-    while(i  >= 0 && v > valor){
-        v[i+1] = v[i];
-        i --;
+    
+    while (i >= 0 && v[i] > valor) {
+        v[i + 1] = v[i];
+        i--;
     }
-    v [i+1] = valor;
+    // Inserir o valor na posição correta
+    v[i + 1] = valor;
 }
 
-int main(){
+int main() {
     int qtd;
 
-    printf("Digite a quantidade de valores : ");
-    scanf("%d",&qtd);
+    printf("Digite a quantidade de valores: ");
+    scanf("%d", &qtd);
 
-    if(qtd <= 0){
+    if (qtd <= 0) {
         printf("Quantidade invalida.\n");
-        return 1; 
-    
+        return 1;
+    }
+
     int v[qtd];
     int valor;
 
-    for(int i = 0; i < qtd; i++){
-        printf("Digite o valor %d :", i + 1);
-        scanf("%d",&valor);
-        ordenacao(v,i,valor);
+    for (int i = 0; i < qtd; i++) {
+        printf("Digite o valor %d: ", i + 1);
+        scanf("%d", &valor);
+        ordenacao(v, i, valor);
     }
+
     printf("Vetor ordenado: ");
-    for(int i = 0; i < qtd; i++){
-        printf("%d,",v[i]);
+    for (int i = 0; i < qtd; i++) {
+        printf("%d ", v[i]);
     }
     printf("\n");
 
