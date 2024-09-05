c3129cb504dac602fb3207e2189beba02ea90c89
pedro-hma <105680915+pedro-hma@users.noreply.github.com>
2024-09-05 11:24:14 +0000

    solicitação de digitação

--- atividade 02/atv02.c
+++ atividade 02/atv02.c
@@ -1,7 +1,7 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-# define CAPACIDADE_VETOR 10
+# define CAPACIDADE_VETOR 50
 
 void imprimir_vetor(int v[],int qtd);
 void insertionSort(int v[],int qtd);
@@ -10,9 +10,27 @@ int main(){
     char opcao;
     int qtd,num,pos;
     int v[CAPACIDADE_VETOR];
-
+    // solicita para o usuario inserir o tamanho do vetor
     do{
-        printf("Qual o tamanho do vetor ?(valor entre 1 e %d) ",CAPACIDADE_VETOR);
+        printf("Qual o tamanho do vetor ?(valor entre 3t e %d) ",CAPACIDADE_VETOR);
         scanf("%d",&qtd);
     } while(qtd < 1 || qtd > CAPACIDADE_VETOR);
+    // solicita para o digitar os valores do vetor
+    printf("Digitação dos valores no vetor\n :");
+    for(int i = 0; i < qtd; i++){
+        printf("Digite o %dº numero :",i+1);
+        scanf("%d",&v[i]);
+    }
+    printf("\n Impressão do vetor original\n :");
+    printf("V =");
+
+    // chamada das funções 
+    imprimir_vetor(v,qtd);
+
+    insertionSort(v,qtd);
+
+    // impressao do vetor de formar ordenada
+    printf("V =");
+    imprimir_vetor(v,qtd);
+
 }
