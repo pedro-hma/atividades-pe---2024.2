d1d5c02580c3ba4c42f065e76a9050b6ffe19461
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2024-09-16 11:32:52 +0000

    inicial commit atv4

---
+++ atividade 04/atv4.c
@@ -0,0 +1,11 @@
+# include  <stdio.h>
+
+void multiplicarMatrizes(matrizA[QTD_LINHAS][QTD_COLUNAS], int linA, int colA, int matrizB[QTD_LINHAS][QTD_COLUNAS], int linA, colB, matrizMult[QTD_LINHAS][QTD_COLUNAS]);
+
+int main(){
+    int linhaA,colunaA,linhaB,colunaB;
+    int matA[QTD_LINHAS][],matB;
+
+
+    return 0;
+}
