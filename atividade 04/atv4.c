d07d65fae68be1421c64e4e8797d0b0efed64fa2
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2024-09-16 11:47:55 +0000

    correção dos erros

--- atividade 04/atv4.c
+++ atividade 04/atv4.c
@@ -1,10 +1,15 @@
 # include  <stdio.h>
+# include <stdlib.h>
+# include <time.h>
 
-void multiplicarMatrizes(matrizA[QTD_LINHAS][QTD_COLUNAS], int linA, int colA, int matrizB[QTD_LINHAS][QTD_COLUNAS], int linA, colB, matrizMult[QTD_LINHAS][QTD_COLUNAS]);
+# define QTD_LINHAS 100
+# define QTD_COLUNAS 100
+
+void multiplicarMatrizes(int matrizA[][QTD_COLUNAS], int linA, int colA, int matrizB[QTD_LINHAS][QTD_COLUNAS], int colB, int matrizMult[QTD_LINHAS][QTD_COLUNAS]);
 
 int main(){
     int linhaA,colunaA,linhaB,colunaB;
-    int matA[QTD_LINHAS][],matB;
+    int matrizA[QTD_LINHAS][QTD_COLUNAS];
 
 
     return 0;
