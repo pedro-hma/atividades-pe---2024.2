38796db3e3e0c37bc937a7e7e620c3519a67229e
pedro-hmayres <105680915+pedro-hma@users.noreply.github.com>
2024-09-20 13:54:00 -0300

    Update atv2450.c

--- atividades beecrowd/atv2450.c
+++ atividades beecrowd/atv2450.c
@@ -1,7 +1,58 @@
-# include <stdio.h>
-# include <stdlib.h>
+#include <stdio.h>
 
-# define QTD_LINHAS 100
-# define QTD_COLUNAS 100
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
 
-void matrizEscada(int mat[QTD_COLUNAS])
+    int matriz[N][M];
+
+    // Leitura da matriz
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    // Verificação se a matriz está na forma escada
+    char resultado = 'S'; // Inicialmente assumimos que está na forma escada
+
+    for (int i = 0; i < N; i++) {
+        int encontrado = 0; // Flag para indicar se foi encontrado um elemento diferente de zero
+        for (int j = 0; j < M; j++) {
+            if (matriz[i][j] != 0) {
+                encontrado = 1; // Elemento diferente de zero encontrado
+                // Verifica se todos os elementos à esquerda de matriz[i][j] são zero
+                for (int k = 0; k < j; k++) {
+                    if (matriz[i][k] != 0) {
+                        resultado = 'N'; // Não está na forma escada
+                        break;
+                    }
+                }
+                for (int k = i + 1; k < N; k++) {
+                    if (matriz[k][j] != 0) {
+                        resultado = 'N';
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        if (!encontrado) {
+            for (int k = i + 1; k < N; k++) {
+                for (int l = 0; l < M; l++) {
+                    if (matriz[k][l] != 0) {
+                        resultado = 'N';
+                        break;
+                    }
+                }
+            }
+        }
+        if (resultado == 'N') {
+            break;
+        }
+    }
+
+    printf("%c\n", resultado);
+
+    return 0;
+}
